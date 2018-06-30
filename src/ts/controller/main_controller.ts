b45106be4a057ad0602871f1551e5d50ce32f8dc
Furmanus <mietek76@gmail.com>
2018-06-30 22:46:02 +0200

    plot removed

--- src/ts/controller/main_controller.ts
+++ src/ts/controller/main_controller.ts
@@ -1,7 +1,5 @@
 import {CalculatorController} from './calculator_controller';
-import {PlotController} from './plot_controller';
 
 export class MainController {
     private calculatorController: CalculatorController = new CalculatorController();
-    private plotController: PlotController = new PlotController();
 }
